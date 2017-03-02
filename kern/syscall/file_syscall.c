19dab9e45b0fda1b719dcbbeb699843a7762624c
Jihai <jihaixu@gmail.com>
2017-03-02 04:11:03 +0000

    file_write

--- kern/syscall/file_syscall.c
+++ kern/syscall/file_syscall.c
@@ -16,26 +16,26 @@ sys_write(int fd, const void *buffer, int len)
 	struct iovec iov;
 	struct uio u;
 	int result;
-	
+
 	struct vnode *v;
 	char console[5] = "con:";
 	(void)fd;
-	
-	//kprintf("BEGIN TO WRITE!!!");
-	
-	/* Initialize uio */
-	//iov.iov_ubase = (userptr_t)buffer;
-	//iov.iov_len = len;
-	//u.uio_iov = &iov;
-	//u.uio_iovcnt = 1;
-	//u.uio_resid = len;
-	//u.uio_segflg = UIO_USERSPACE;
-	//u.uio_rw = UIO_WRITE;
-	//u.uio_space = curproc->p_addrspace;
-	
-	uio_kinit(&iov, &u, (userptr_t)buffer, len, 0, UIO_WRITE);
 
-	result = vfs_open(console, O_WRONLY, 0664, &v);
+	// kprintf("BEGIN TO WRITE!!!");
+
+	/* Initialize uio */
+	iov.iov_ubase = (userptr_t)buffer;
+	iov.iov_len = len;
+	u.uio_iov = &iov;
+	u.uio_iovcnt = 1;
+	u.uio_resid = len;
+	u.uio_segflg = UIO_USERSPACE;
+	u.uio_rw = UIO_WRITE;
+	u.uio_space = curproc->p_addrspace;
+
+	// uio_kinit(&iov, &u, (userptr_t)buffer, len, 0, UIO_WRITE);
+
+	result = vfs_open(console, O_WRONLY, 0, &v);
 	if (result){
 		vfs_close(v);
 		return result;
